c466569e3f11947bd8e3c03f78272742c8956027
Candy <Jazmincandy2224@gmail.com>
2021-02-23 15:38:26 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Proyectosc_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n---------------------------------------------------\n");
+            Console.WriteLine("                     Hola Mundo!                   \n");
+            Console.WriteLine("---------------------------------------------------\n");
+        }
+    }
+}
